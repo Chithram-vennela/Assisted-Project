5b0c6915322b1ff875550d3fbe6093e8629d68d1
Chithram-vennela <99874140+Chithram-vennela@users.noreply.github.com>
2022-03-02 11:54:14 +0530

    Types of methods

---
+++ Methods/src/Model/method.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class method {
+	public int multipynumbers(int a,int b) {
+		int m=a*b;
+		return m;
+	}
+
+	public static void main(String[] args) {
+		method b=new method();
+		int ans= b.multipynumbers(2,8);
+		System.out.println("Multipilcation is :"+ans);
+		
+
+
+	}
+
+}
