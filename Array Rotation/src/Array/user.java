e1fce79aeb38ca686676fe8c89de6b3147553c89
Chithram-vennela <99874140+Chithram-vennela@users.noreply.github.com>
2022-03-05 19:03:04 +0530

    Array Rotation

---
+++ Array Rotation/src/Array/user.java
@@ -0,0 +1,12 @@
+package Array;
+public class user
+{
+	public static void main(String[] args) {
+		RotationArray r = new RotationArray();
+        		int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        		r.rotate(arr, 5); 
+        		for(int i=0;i<arr.length;i++){
+            			System.out.print(arr[i]+" ");
+        		}
+	}
+}
